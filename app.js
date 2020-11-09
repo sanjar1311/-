0737d98e676d65507cce14918690c5ed062b39f3
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-11-09 10:42:29 +0500

    add js function to article section

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ toggler.addEventListener('click', function(){
 toggler.addEventListener('click', function(){
   toggler.classList.toggle('burger__close');
 });
+
+var articleList = document.querySelector('.article__article-right');
+var articleOpenBtn = document.querySelector('.article-right__buttons--left-btn');
+var articleCloseBtn = document.querySelector('.article-right__buttons--right-btn');
+
+articleOpenBtn.addEventListener('click', function(){
+  articleList.classList.remove('article-right--open');
+});
+
+articleCloseBtn.addEventListener('click', function(){
+  articleList.classList.add('article-right--open');
+});
+
+articleCloseBtn.addEventListener('click', function(){
+  articleOpenBtn.classList.remove('article-right__btn-active');
+});
+
+articleCloseBtn.addEventListener('click', function(){
+  articleCloseBtn.classList.add('article-right__btn-active');
+});
+
+articleOpenBtn.addEventListener('click', function(){
+  articleCloseBtn.classList.remove('article-right__btn-active');
+});
+
+articleOpenBtn.addEventListener('click', function(){
+  articleOpenBtn.classList.add('article-right__btn-active');
+});
